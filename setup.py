18757350b452f096652cf1c62d736fa75de939c6
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-31 20:17:57 +0200

    Fix version for download tarball

--- setup.py
+++ setup.py
@@ -2,27 +2,33 @@
 
 from distutils.core import setup
 
-setup(name='kivyunittest',
-      version='0.1.4',
-      description='Unittesting for Kivy framework',
-      author='Peter Badida',
-      author_email='keyweeusr@gmail.com',
-      url='https://github.com/KeyWeeUsr/KivyUnitTest',
-      download_url='https://github.com/KeyWeeUsr/KivyUnitTest/tarball/0.1.1',
-      packages=['kivyunittest'],
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Software Development :: Bug Tracking',
-          'Topic :: Software Development :: Testing',
-      ],
-      keywords=['unittest', 'testing', 'debug', 'kivy'],
-      license="License :: OSI Approved :: MIT License",
-      )
+version = '0.1.4'
+
+setup(
+    name='kivyunittest',
+    version=version,
+    description='Unittesting for Kivy framework',
+    author='Peter Badida',
+    author_email='keyweeusr@gmail.com',
+    url='https://github.com/KeyWeeUsr/KivyUnitTest',
+    download_url=(
+        'https://github.com/KeyWeeUsr/KivyUnitTest/tarball/'
+        '{}'.format(version)
+    ),
+    packages=['kivyunittest'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Bug Tracking',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords=['unittest', 'testing', 'debug', 'kivy'],
+    license="License :: OSI Approved :: MIT License",
+)
