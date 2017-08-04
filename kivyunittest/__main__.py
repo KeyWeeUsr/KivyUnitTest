1296b659b4b5658e03f33838e997d9612abce71a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-08-04 23:49:32 +0200

    Rewrite custom args handling to argparse

--- kivyunittest/__main__.py
+++ kivyunittest/__main__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # KivyUnitTest
-# Version: 0.1.5
 # Copyright (C) 2016 - 2017, KeyWeeUsr(Peter Badida) <keyweeusr@gmail.com>
 # License: The MIT License (MIT)
 #
@@ -28,68 +27,120 @@
 # Each test _needs_ a fresh python, otherwise only a single test will run
 
 from __future__ import print_function
+from argparse import ArgumentParser
 
 import os
 import sys
 import os.path as op
 from time import time
 import subprocess as subp
-from os.path import isfile
+from os.path import isfile, join, dirname, abspath
 from os import listdir as ls
+
+__version__ = '0.1.6'
+
+
+class Parser(ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super(Parser, self).__init__(
+            prog='KivyUnitTest',
+            epilog=(
+                'WARNING: if there are files with the same name, '
+                'only the file in the first folder will be run!'
+            )
+        )
+
+        self.add_argument(
+            '-V', '--version',
+            action='version',
+            version='%(prog)s %(ver)s' % {
+                'prog': self.prog, 'ver': __version__
+            }
+        )
+        self.add_argument(
+            '-v', '--verbose', action='count', default=0,
+            help=(
+                'increase verbosity of the Kivy output, '
+                'can stack up to 2 times to achieve different log output'
+            )
+        )
+        self.add_argument(
+            '--demo', action='store_true',
+            help=(
+                'run demo for the %(prog)s'
+            )
+        )
+        self.add_argument(
+            '-d', '--folder',
+            type=str, nargs='+', metavar='DIR', default=[],
+            help=(
+                'list of folders with test files, '
+                'defaults to: %(default)s'
+            ),
+        )
+        self.add_argument(
+            '-p', '--pythonpath',
+            type=str, nargs='+', metavar='DIR',
+            default=[
+                dirname(abspath(__file__))
+            ],
+            help=(
+                'list of non-standard paths to import modules from, '
+                'defaults to: %(default)s'
+            )
+        )
 
 
 class Test(object):
     def __init__(self, **kwargs):
         super(Test, self).__init__(**kwargs)
-        self._path = op.dirname(op.abspath(__file__))
+
         self.path = []
-        try:
-            self.path.append(self._path)
-            if '--folder' in sys.argv:
-                pos = sys.argv.index('--folder') + 1
-                path = sys.argv[pos]
-                if path.startswith('\'') or path.startswith('"'):
-                    path = path[1:]
-                if path.endswith('\'') or path.endswith('"'):
-                    path = path[:-1]
-                path = path.strip('.py')
-                self.path.append(path)
-
-                self.modules = []
-                m = [f for f in ls(path) if isfile(op.join(path, f))]
-                for mod in m:
-                    if (mod.startswith('test_') and mod.endswith('.py')):
-                        self.modules.append(mod.strip('.py'))
-            else:
-                self.modules = ['test_text', ]
-            if '--pythonpath' in sys.argv:
-                pos = sys.argv.index('--pythonpath') + 1
-                path = sys.argv[pos]
-
-                if path.startswith('\'') or path.startswith('"'):
-                    path = path[1:]
-                if path.endswith('\'') or path.endswith('"'):
-                    path = path[:-1]
-                self.path.append(path)
-        except IndexError:
-            self.path = [self._path]
-            self.modules = ['test_text', ]
+        self.modules = []
         self.outputs = []
+        self.log_level = ['info', 'debug', 'trace']
+
+        # override --folder & --pythonpath args if running demo
+        if parse.demo:
+            parse.folder = [
+                join(dirname(abspath(__file__)), 'examples')
+            ]
+            parse.pythonpath = [
+                dirname(abspath(__file__))
+            ]
+
+        self.path.extend(parse.folder)
+
+        mods = [
+            f for path in self.path for f in ls(path) if isfile(join(path, f))
+        ]
+        for mod in mods:
+            if (mod.startswith('test_') and mod.endswith('.py')):
+                self.modules.append(mod.strip('.py'))
+
+        if parse.pythonpath:
+            # optional arg path first
+            self.path = parse.pythonpath + self.path
 
     def run(self):
         self.startTime = time()
 
         for mod in self.modules:
             args = [
-                'python', '-c',
-                ('import sys; import os.path as op;'
-                 'import unittest;'
-                 'sys.path.extend(%s);'
-                 'loader = unittest.TestLoader();'
-                 'suite = loader.loadTestsFromName("%s");'
-                 'runner = unittest.TextTestRunner(verbosity=2);'
-                 'result = runner.run(suite);'
-                 ' ' % (repr(self.path), mod)
+                'python', '-c', (
+                    'import sys;'
+                    'import unittest;'
+                    'from kivy.config import Config;'
+                    'sys.path = %(path)s + sys.path;'
+                    'Config.set("kivy", "log_level", "%(level)s");'
+                    'loader = unittest.TestLoader();'
+                    'suite = loader.loadTestsFromName("%(module)s");'
+                    'runner = unittest.TextTestRunner(verbosity=0);'
+                    'result = runner.run(suite);' % {
+                        'path': repr(self.path),
+                        'module': mod,
+                        'level': self.log_level[parse.verbose]
+                    }
                 )
             ]
 
@@ -154,4 +205,5 @@ class Test(object):
             print('SUCCESS!')
 
 if __name__ == '__main__':
+    parse = Parser().parse_args()
     Test().run()
