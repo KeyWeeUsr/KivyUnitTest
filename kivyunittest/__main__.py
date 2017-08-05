e8fc290d298a58b9378f398078719208594f539e
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-08-05 21:52:39 +0200

    Cleaning, commenting, ironing

--- kivyunittest/__main__.py
+++ kivyunittest/__main__.py
@@ -24,18 +24,16 @@
 # LICENSE.txt file shall be included in all copies or substantial portions
 # of the Software.
 
-# Each test _needs_ a fresh python, otherwise only a single test will run
-
 from __future__ import print_function
 from argparse import ArgumentParser
 
 import os
 import sys
-import os.path as op
-from time import time
 import subprocess as subp
-from os.path import isfile, join, dirname, abspath
+
+from time import time
 from os import listdir as ls
+from os.path import isfile, join, dirname, abspath
 
 __version__ = '0.1.6'
 
@@ -97,7 +95,6 @@ class Test(object):
 
         self.path = []
         self.modules = []
-        self.outputs = []
         self.log_level = ['info', 'debug', 'trace']
 
         # override --folder & --pythonpath args if running demo
@@ -111,48 +108,57 @@ class Test(object):
 
         self.path.extend(parse.folder)
 
+        # get files only from requested folders
         mods = [
             f for path in self.path for f in ls(path) if isfile(join(path, f))
         ]
+
+        # unittest.TestLoader.loadTestsFromName doesn't want raw filenames
         for mod in mods:
             if (mod.startswith('test_') and mod.endswith('.py')):
                 self.modules.append(mod.strip('.py'))
 
+        # put desired folders with modules first
         if parse.pythonpath:
-            # optional arg path first
             self.path = parse.pythonpath + self.path
 
     def run(self):
         self.startTime = time()
+        outputs = []
 
+        # run for each fetched module separate interpreter
         for mod in self.modules:
             args = [
                 'python', '-c', (
                     'import sys;'
                     'import unittest;'
                     'from kivy.config import Config;'
-                    'sys.path = %(path)s + sys.path;'
+                    'sys.path += %(path)s;'
                     'Config.set("kivy", "log_level", "%(level)s");'
                     'loader = unittest.TestLoader();'
                     'suite = loader.loadTestsFromName("%(module)s");'
                     'runner = unittest.TextTestRunner(verbosity=0);'
                     'result = runner.run(suite);' % {
+                        'level': self.log_level[parse.verbose],
                         'path': repr(self.path),
-                        'module': mod,
-                        'level': self.log_level[parse.verbose]
+                        'module': mod
                     }
                 )
             ]
 
+            # if something happens, always preserve the output
             try:
                 call = [subp.check_output(args, stderr=subp.STDOUT)]
             except subp.CalledProcessError as exc:
                 call = [exc.output]
-            self.outputs.append(call)
+            outputs.append(call)
+        self.trim_output(outputs)
 
+    def trim_output(self, outputs):
         # remove separators
         cleaned = []
-        for output in self.outputs:
+        while True:
+            output = outputs.pop()
             _temp = []
             for out in output:
                 try:
@@ -160,11 +166,12 @@ class Test(object):
                 except TypeError:
                     _temp.append(out.split(os.linesep.encode('utf-8')))
             cleaned.append(_temp)
-        self.outputs = cleaned[:]
+            if not outputs:
+                break
 
         # get errors' log
         errors = []
-        for i, output in enumerate(self.outputs):
+        for i, output in enumerate(cleaned):
             for j, lines in enumerate(output):
                 for line in lines:
                     try:
@@ -181,7 +188,7 @@ class Test(object):
             print('='*70)
             print('|', error[2], ' '*(61-len(error[2])), 'LOG |')
             print('='*70)
-            for line in self.outputs[error[0]][error[1]]:
+            for line in cleaned[error[0]][error[1]]:
                 if sys.version_info[0] == 3:
                     print(line.decode('utf-8'))
                 else:
@@ -203,6 +210,7 @@ class Test(object):
             exit(1)
         else:
             print('SUCCESS!')
+
 
 if __name__ == '__main__':
     parse = Parser().parse_args()
